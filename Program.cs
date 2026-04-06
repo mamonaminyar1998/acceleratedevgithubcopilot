97331aecff196dade6a1022b16b79d88f3c90a56
mamon aminyar <203014904+mamonaminyar1998@users.noreply.github.com>
2026-04-07 00:33:03 +0430

    Add Program.cs file with initial setup for weather dashboard application.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<IWeatherService, OpenWeatherService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseStaticFiles();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
